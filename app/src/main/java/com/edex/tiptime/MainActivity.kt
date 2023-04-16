444f8ec42f847adf0a9c49a703e7ec8a1f2c17d9
Hamza <hamza.afzalkhan9@gmail.com>
2023-04-17 01:38:15 +0500

    change in string resource and main.kt file

--- app/src/main/java/com/edex/tiptime/MainActivity.kt
+++ app/src/main/java/com/edex/tiptime/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.edex.tiptime
 
 import android.os.Bundle
-import android.widget.Switch
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
@@ -19,6 +18,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -29,6 +29,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
@@ -38,7 +39,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.view.ViewCompat
 import com.edex.tiptime.ui.theme.TipTimeTheme
 import java.text.NumberFormat
 
@@ -84,7 +84,7 @@ fun TipTimeScreen() {
                 imeAction = ImeAction.Next
             ),
             keyboardActions = KeyboardActions(
-                onNext = { focusManager.moveFocus(ViewCompat.FocusDirection.Down) }
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
             ),
             value = amountInput,
             onValueChanged = { amountInput = it }
